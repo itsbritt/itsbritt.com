d2d8ecad7299157500f3002936d31d564f7eab88
britt shroyer <britt.shroyer@sailpoint.com>
2021-02-24 00:00:34 -0600

    Create sort buttons

--- src/pages/digital_garden.js
+++ src/pages/digital_garden.js
@@ -3,14 +3,14 @@ import { Link } from 'gatsby';
 import {FaLeaf, FaSeedling, FaTree} from 'react-icons/fa';
 
 // React
-import React from 'react';
+import React, { useReducer } from 'react';
 
 // Components
 import Layout from '../components/Layout/Layout';
 
-const PostSummary = ({ postDetails }) => {
-	const { title, growthState, dateTended } = postDetails.node.childMarkdownRemark.frontmatter;
-	const { slug } = postDetails.node.childMarkdownRemark.fields;
+const PostSummary = ({ postdetails }) => {
+	const { title, growthstate, datetended } = postdetails.node.childmarkdownremark.frontmatter;
+	const { slug } = postdetails.node.childmarkdownremark.fields;
 	const icons = {
 		'seedling': <FaSeedling />,
 		'budding': <FaLeaf />,
@@ -18,41 +18,68 @@ const PostSummary = ({ postDetails }) => {
 	};
 	return (
 		<div className="post-summary" style={{ marginBottom: '3rem' }}>
-			<Link to={slug}>{title}</Link>
-			<span><i className="fas fa-seedling"></i></span>
-			<span className="post-date">{dateTended}</span>
-			<div>
-				{
-					icons[growthState]
-				}
+			<link to={slug}>{title}</link>
+			<div style={{display: 'flex', alignItems: 'center', marginTop: '0.5rem'}}>
+				<span>{datetended}</span>
+				<div style={{marginLeft: '1rem'}}>
+					{
+						icons[growthstate]
+					}
+				</div>
 			</div>
 		</div>
 	);
 };
-
+const initialState = {"seedling": true, "budding": true, "evergreen": true};
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'seedling':
+			return {...state, 'seedling': !state['seedling']};
+		case 'budding':
+			return {...state, 'budding': !state['budding']};
+		case 'evergreen':
+			return {...state, 'evergreen': !state['evergreen']};
+		default:
+			throw new Error();
+	}
+};
 const DigitalGarden = ({ data }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
 	return (
 		<Layout>
 			<div className="page-body-container">
-				<p className="page-header">Digital Garden</p>
+				<p className="page-header">digital garden</p>
 				<p style={{ marginBottom: '3rem' }}>
-					Labore magna mollit voluptate id incididunt ut consequat amet enim consectetur sunt. Fugiat minim
-					laboris mollit adipisicing id mollit deserunt id qui eu sunt id culpa exercitation. Occaecat officia
-					ut nostrud magna et Lorem voluptate magna laborum quis ex ut. Magna excepteur qui ex mollit commodo.
+					labore magna mollit voluptate id incididunt ut consequat amet enim consectetur sunt. fugiat minim
+					laboris mollit adipisicing id mollit deserunt id qui eu sunt id culpa exercitation. occaecat officia
+					ut nostrud magna et lorem voluptate magna laborum quis ex ut. magna excepteur qui ex mollit commodo.
 				</p>
 				<div className="post-filters">
-					<ul className="post-filters__topic">Tags Here</ul>
+					<ul className="post-filters__topic">tags here</ul>
 					<ul className="post-filters__state">
-						<li>Seedling</li>
-						<li>Budding</li>
-						<li>Evergreen</li>
+						<li>
+							<button onclick={() => dispatch({type: 'seedling'})}>Seedling</button>
+						</li>
+						<li>
+							<button onclick={() => dispatch({type: 'budding'})}>Budding</button>
+						</li>
+						<li>
+							<button onclick={() => dispatch({type: 'evergreen'})}>Evergreen</button>
+						</li>
 					</ul>
 				</div>
-				<div className="post-list">
-					{data.allFile.edges.map((edge, i) => (
-						<PostSummary postDetails={edge} key={i} />
-					))}
+				{/* <div classname="selected-state">
+					<ul>
+						{state.map((selectedstate, i) => <li key={i}>
+							icons[selectedstate]
+						</li>)}
+					</ul>
 				</div>
+				<div classname="post-list">
+					{data.allFile.edges.map((edge, i) => (
+						<postsummary postdetails={edge} key={i} />
+					))}
+				</div> */}
 			</div>
 		</Layout>
 	);
@@ -61,7 +88,7 @@ const DigitalGarden = ({ data }) => {
 export default DigitalGarden;
 
 export const query = graphql`
-	query Posts {
+	query posts {
 		allFile(
 			filter: { sourceInstanceName: { eq: "content" }, dir: { regex: "/content/digital_garden/" } }
 			sort: { fields: childMarkdownRemark___frontmatter___dateTended, order: DESC }
@@ -75,7 +102,7 @@ export const query = graphql`
 						frontmatter {
 							title
 							growthState
-							dateTended(formatString: "MM.DD.YY")
+							datePlanted(formatString: "mm.dd.yy")
 						}
 					}
 				}
